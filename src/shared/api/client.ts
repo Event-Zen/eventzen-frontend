0fa0d4d32f808f3db2c7ce2a9b620a9e1dd56c67
bhashana99 <bhashanachamodya99@gmail.com>
2026-03-04 23:58:04 +0530

    function for auto add Authorization header

---
+++ src/shared/api/client.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_USER_SERVICE_URL || "http://localhost:4001",
+  withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token) {
+    config.headers = config.headers ?? {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});

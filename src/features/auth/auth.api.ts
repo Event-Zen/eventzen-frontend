a9a4bdc9208faf19bf849c497541ca092800222e
bhashana99 <bhashanachamodya99@gmail.com>
2026-03-04 22:24:31 +0530

    add login Api to api

--- src/features/auth/auth.api.ts
+++ src/features/auth/auth.api.ts
@@ -5,3 +5,8 @@ export async function registerApi(payload: RegisterRequest) {
   const res = await api.post<RegisterResponse>("/api/auth/register", payload);
   return res.data;
 }
+
+export async function loginApi(payload: { email: string; password: string }) {
+  const res = await api.post("/api/auth/login", payload);
+  return res.data; // { accessToken, user }
+}

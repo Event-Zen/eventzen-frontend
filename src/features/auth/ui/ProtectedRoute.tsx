4c0311cef83ad19a79063d974256ca8a6c718a68
bhashana99 <bhashanachamodya99@gmail.com>
2026-03-05 00:00:30 +0530

    create ProtectedRoute component

---
+++ src/features/auth/ui/ProtectedRoute.tsx
@@ -0,0 +1,8 @@
+import type { JSX } from "react";
+import { Navigate } from "react-router-dom";
+
+export function ProtectedRoute({ children }: { children: JSX.Element }) {
+  const token = localStorage.getItem("accessToken");
+  if (!token) return <Navigate to="/login" replace />;
+  return children;
+}
